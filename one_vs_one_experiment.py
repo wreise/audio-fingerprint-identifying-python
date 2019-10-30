92613dec2e0e8d83f3c447975c39ca737b18ea50
wreise <wojciech.reise@epfl.ch>
2019-10-30 19:30:29 +0100

    Worked on the 1vs1 experiment - parallelize and it's ready to run (use a queue instead of a list)

--- one_vs_one_experiment.py
+++ one_vs_one_experiment.py
@@ -9,14 +9,31 @@ from tqdm import tqdm as tqdm
 
 from multiprocessing import Pool
 
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
+
+def get_dict_for_file_local(s_name):
+    try:
+        return fingerprint.get_dict_for_file('../spotify_dataset/data/'+s_name+'.ogg')
+    except:
+        return fingerprint.get_dict_for_file('../spotify_dataset/transformed/'+s_name+'.ogg')
+
 def process_pair(s_name1, s_name2):
-    song_1, song_2 = [fingerprint.get_dict_for_file(f) for f in [s_name1, s_name2]]
+    song_1, song_2 =  [get_dict_for_file_local(s) for s in [s_name1, s_name2]]
 
     common_keys = set(song_1).intersection(set(song_2))
     offsets = [[song_1[c], song_2[c]] for c in common_keys]
 
     mode, count = stats.mode(np.diff(offsets, axis = 1), axis = None)
-    return (s_name1, s_name2, offsets), (s_name1, s_name2, mode, count)
+    return (s_name1, s_name2, offsets), (s_name1, s_name2, mode[0], count[0])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -25,16 +42,11 @@ if __name__ == '__main__':
 
     pairs_df = pd.read_pickle(args.file)
 
-    all_alignments = []
-    all_matches = {}
-    for pair in pairs_df:
+    results = []
+    for (_, pair), _ in zip(pairs_df.iterrows(), range(5)):
         id1, id2 = pair.id1, pair.id2
         matches, alignment = process_pair(id1, id2)
-        all_matches.update({(id1, id2): matches})
-        all_alignments.append({c: cv for c,cv in zip(['id1','id2', 'alignment', 'count'], alignment})
-
-    with open('one_vs_one_matches.json', 'wb+') as f:
-        json.dumps(all_matches)
-
-    with open('one_vs_one_alignments.json', 'wb+') as f:
-        json.dumps(all_alignments)
+        results.append({c: cv for c,cv in zip(['id1','id2', 'alignment', 'count', 'matches'], [*alignment, matches[2]])})
+        
+    with open('one_vs_one_alignments.json', 'w') as f_alignments:
+        json.dump(results, f_alignments, cls = NpEncoder)
